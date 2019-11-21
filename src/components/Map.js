00a9cbf80b263e4dd2a9161296c6dabcd3be9d5f
nonakarapetyan <nona.karapetyan.98@mail.ru>
2019-11-21 18:31:52 +0400

    current position on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,11 +8,16 @@ const MapBox = ReactMapboxGl({
 
 const Map = () => {
     const [marker, setMarker] = useState(null)
+    const [current, setCurrent] = useState(null)
 
     const onMapClick = (e, data) => {
         setMarker(data.lngLat)
     }
-
+    navigator.geolocation.getCurrentPosition((position)=>{
+       setCurrent(position.coords)
+       console.log(position)
+    })
+   
     return (
         <MapBox
             style="mapbox://styles/mapbox/streets-v9"
@@ -23,27 +28,45 @@ const Map = () => {
                 height: '500px',
                 width: '100%'
             }}>
-                {marker && (
+               
+                {marker  && (
                     <Marker 
-                        coordinates={[marker.lng, marker.lat]}
-                        onClick={() => {
-                            // 
-                        }}>
-                            <img src="/images/marker.svg" height="50px"/>
+                    coordinates={[marker.lng, marker.lat]}
+                    onClick={() => {
+                        // 
+                    }}>
+                    <img src="/images/marker.svg"/>
                     </Marker>
                 )}
+                        {
+                            current && (
+                                <Marker
+                                coordinates={[current.longitude, current.latitude]}
+                                onClick={()=>{
+                                        //
+                                }}>
+                                    <div>
+                                    <img src="/images/pin.png" height="50px"/>
+                                    </div>
+                                </Marker>
+                            )
+                            
+                        }
                 {/* <Popup
                     coordinates={[45.3164576, 39.7629985]}
                     offset={{
                         'bottom-left': [12, -38],  'bottom': [0, -38], 'bottom-right': [-12, -38]
                     }}>
                         <h1>Popup</h1>
-                    </Popup> */}
-                {/* <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+                    </Popup> 
+             <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
                     <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-                </Layer> */}
+                </Layer>  */}
         </MapBox>
     )
 }
 
 export default Map
+
+
+
